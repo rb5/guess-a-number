1b17851fc60f9f80e2ade0a47d897be9b225d940
rb5 <33368303+rb5@users.noreply.github.com>
2017-11-12 16:04:35 +0300

    Add files via upload

---
+++ guess-a-number.py
@@ -0,0 +1,24 @@
+﻿# guess a number
+print('a number from 0 to 100 is envisioned')
+import random
+o = 'y'
+while (o == 'y')or(o == 'yes'):
+    number = random.randint(0,100)
+    print('\nhow much you need to try to guess it? (enter the number of attempts)')
+    k = int(input())
+    a = -1
+    i = 0
+    while (number != a)and(i < k):
+        i += 1
+        print('attempt №%d:' % i)
+        a = int(input())
+        if (a == number):
+            print('right, you won!')
+            break
+        elif (number < a):
+            print(' number less than', a)
+        elif (number > a):
+            print(' number greater than', a)
+    else:
+        print('you lose. did not guess the number %d' % number)
+    o = input('to start the game again? (y/n) ')
